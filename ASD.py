b30bfce16484f11fc00ec8941d587eb800d09aee
Juan Camilo Florez <jcflorezv@unal.edu.co>
2021-04-17 22:23:50 -0500

    added grammar input from file

--- ASD.py
+++ ASD.py
@@ -5,59 +5,18 @@ import pprint
 ##########################################################
 
 def main():
-    GRAMMAR = get_grammar(0)
-    
-    asd = TopDownSyntacticParser(GRAMMAR)
+    grammar, nont_ord = grammar_from_file("03")
+    asd = TopDownSyntacticParser(grammar, nont_ord)
     PRIMEROS = asd.compute_primeros()
     pprint.pprint(PRIMEROS)
     SIGUIENTES = asd.compute_siguientes()
     pprint.pprint(SIGUIENTES)
-
-"""
-se supone la convension de que las reglas con epsilon unico
-se escriben siempre al final
-"""
-# TODO completar el metodo buscando recursivamente en profundidad
-def fix_easy_left_recursion(grammar):
-    nont_with_recusion = {}
-
-    # buscar recusiones por izquierda y si existe epsilon en 
-    # ese no terminal X para solucionar facil
-    for X, rules in grammar.items():
-        wait_e = False
-        i = 0
-        for alpha in rules:
-            # hay recursion por izquierda
-            if alpha[0] == X:
-                wait_e = True
-                print(X + ' has left recursion')
-                # regla a corregir
-                if X not in nont_with_recusion:
-                    nont_with_recusion[X] = [i]
-                else:
-                    nont_with_recusion[X].append[i]
-
-            if wait_e and alpha[0] == 'e':
-                wait_e = False
-                break
-
-            # ubicacion regla, dentro de reglas
-            i += 1
-        
-        # no puedo arreglar facilmente no hay epsilon para
-        # suprimir la recursion
-        if wait_e:
-            raise EnvironmentError
-
-    # corregir la recursividad por izquierda
-    for X, loc in nont_with_recusion.items():
-        print(X, loc)
         
 class TopDownSyntacticParser:
-    def __init__(self, grammar):
+    def __init__(self, grammar, nont_ord):
         self.grammar    = grammar
-        self.nont_set   = set(grammar.keys())
-        self.nont_rev   = list(grammar.keys())
+        self.nont_set   = set(nont_ord)
+        self.nont_rev   = nont_ord
         self.PRIMEROS   = {}
         self.SIGUIENTES = {}
 
@@ -109,7 +68,7 @@ class TopDownSyntacticParser:
     SEGUNDOS
     """
     def compute_siguientes(self):
-        for X in self.nont_set:
+        for X in self.nont_rev:
             self.SIGUIENTES[X].update(self.s(X))
         return self.SIGUIENTES
 
@@ -124,7 +83,7 @@ class TopDownSyntacticParser:
         if nont == self.nont_rev[-1]:
             ans.add('$')
         # iterate over all rules searching for 'nont'
-        for X in self.nont_set:
+        for X in self.nont_rev:
             for rule in self.grammar[X]:
                 if rule.__contains__(nont):
                     loc = rule.index(nont)
@@ -136,67 +95,79 @@ class TopDownSyntacticParser:
                     ans.update(prim_beta)
         return list(ans)
 
-def get_grammar(sample):
-    if sample == 0:
-        # TODO fix case when grammar rule is recursive e.g. A -> A tres ex. 0
-        # TODO fix to allow the computation of SIGUIENTES; ex. 0
-        return {
-            'S' : [
-                ['A', 'uno', 'B', 'C'],
-                ['S', 'dos']
-            ],
-            'A': [
-                ['B', 'C', 'D'],
-                ['A', 'tres'],
-                ['e'],
-            ],
-            'B': [
-                ['D', 'cuatro', 'C', 'tres'],
-                ['e']
-            ],
-            'C': [
-                ['cinco', 'D', 'B'],
-                ['e']
-            ],
-            'D': [
-                ['seis'],
-                ['e']
-            ]
-        }
-    elif sample == 1:
-        return {
-            'A': [
-                ['B', 'C'],
-                ['bad']
-            ],
-            'B': [
-                ['big', 'C', 'boss'],
-                ['bet']
-            ],
-            'C': [
-                ['cat'],
-                ['cow']
-            ]
-        }
-    else:
-        return {
-            'A': [
-                ['B', 'C'],
-                ['ant', 'A', 'all']
-            ],
-            'B': [
-                ['big', 'C'],
-                ['bus', 'A', 'boss'],
-                ['e']
-            ],
-            'C': [
-                ['cat'],
-                ['cow']
-            ]
-        }
+"""
+Lee un archivo y crea un diccionario que representa esa gramatica
+"""
+def grammar_from_file(filename):
+    f = open("grammar/" + filename + ".txt", "r")
+    grammar = {}
+    nont_ord = []
+    nont_set = set()
+    while True:
+        line = f.readline()
+        if not line:
+            break
+
+        if line[0] == '#':
+            continue
+        else:
+            X, rule = line.split(':')
+            
+            X = X.strip()
+            if X not in nont_set:
+                nont_ord.append(X)
+                nont_set.add(X)
+
+            r = rule.strip().split(' ')
+            if X in grammar:
+                grammar[X].append(r)
+            else:
+                grammar[X] = [r]
+
+    f.close()
+    return grammar, nont_ord 
 
 if __name__ == "__main__":
     main()
-    
+
+"""
+se supone la convension de que las reglas con epsilon unico
+se escriben siempre al final
+"""
+# TODO completar el metodo buscando recursivamente en profundidad
+def fix_easy_left_recursion(grammar):
+    nont_with_recusion = {}
+
+    # buscar recusiones por izquierda y si existe epsilon en 
+    # ese no terminal X para solucionar facil
+    for X, rules in grammar.items():
+        wait_e = False
+        i = 0
+        for alpha in rules:
+            # hay recursion por izquierda
+            if alpha[0] == X:
+                wait_e = True
+                print(X + ' has left recursion')
+                # regla a corregir
+                if X not in nont_with_recusion:
+                    nont_with_recusion[X] = [i]
+                else:
+                    nont_with_recusion[X].append[i]
+
+            if wait_e and alpha[0] == 'e':
+                wait_e = False
+                break
+
+            # ubicacion regla, dentro de reglas
+            i += 1
+        
+        # no puedo arreglar facilmente no hay epsilon para
+        # suprimir la recursion
+        if wait_e:
+            raise EnvironmentError
+
+    # corregir la recursividad por izquierda
+    for X, loc in nont_with_recusion.items():
+        print(X, loc)    
 
 
