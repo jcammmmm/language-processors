4bdbcf066477828d1480c6685f2930fa4aa55e69
Juan Camilo Florez <jcflorezv@unal.edu.co>
2021-04-30 23:32:34 -0500

    fix ini, pos and loop grammar

--- globals.py
+++ globals.py
@@ -8,16 +8,16 @@ that lexer without file location will be instanced.
 """
 src_loc = None
 if os.getenv('STAGE') == 'DEVELOPMENT':
-    # src_loc = 'in/a02.psi' # El analisis sintactico ha finalizado exitosamente.
-    # src_loc = 'in/a03.psi' # El analisis sintactico ha finalizado exitosamente.
-    # src_loc = 'in/a04.psi' # El analisis sintactico ha finalizado exitosamente.
-    # src_loc = 'in/a05.psi' # El analisis sintactico ha finalizado exitosamente.
-    # src_loc = 'in/a08.psi' # El analisis sintactico ha finalizado exitosamente.
-    # src_loc = 'in/a11.psi' # <3,17> Error sintactico: se encontro: "0"; se esperaba: "identificador".
-    # src_loc = 'in/a12.psi' # <5,11> Error sintactico: se encontro: ","; se esperaba: ".", ")".
-    # src_loc = 'in/a13.psi' # <8,8> Error sintactico: se encontro: "res"; se esperaba: "entonces".
-    src_loc = 'in/a16.psi' # <6,5> Error sintactico: se encontro: "fin_mientras"; se esperaba: "identificador", "...
-    # src_loc = 'in/a17.psi' # 
+    src_loc = 'in/a02.psi' # El analisis sintactico ha finalizado exitosamente.
+    src_loc = 'in/a03.psi' # El analisis sintactico ha finalizado exitosamente.
+    src_loc = 'in/a04.psi' # El analisis sintactico ha finalizado exitosamente.
+    src_loc = 'in/a05.psi' # El analisis sintactico ha finalizado exitosamente.
+    src_loc = 'in/a08.psi' # El analisis sintactico ha finalizado exitosamente.
+    src_loc = 'in/a11.psi' # <3,17> Error sintactico: se encontro: "0"; se esperaba: "identificador".
+    src_loc = 'in/a12.psi' # <5,11> Error sintactico: se encontro: ","; se esperaba: ".", ")".
+    src_loc = 'in/a13.psi' # <8,8> Error sintactico: se encontro: "res"; se esperaba: "entonces".
+    src_loc = 'in/a16.psi' # <9,1> Error sintactico: se encontro: "fin_principal"; se esperaba: "identificador", "leer", "imprimir", "booleano", "caracter", "entero", "real", "cadena", "si", "mientras", "hacer", "para", "seleccionar", "romper".
+    src_loc = 'in/a17.psi' # <2,5> Error sintactico: se encontro: "hacer"; se esperaba: "booleano", "caracter", "entero", "real", "cadena", "fin_estructura".
     # src_loc = 'in/a18.psi' # 
     # src_loc = 'in/a19.psi' # 
     # src_loc = 'in/a20.psi' # 
